3208e0c7c032d2846f0f50c2dc9e3b9663a78658
mohanad <mohanad_u@yahoo.com>
2024-09-10 10:29:39 +0300

    update (try & catch)

--- src/services/productService.ts
+++ src/services/productService.ts
@@ -5,13 +5,18 @@ export const getAllProducts = async () => {
 };
 
 export const seedInitialProducts = async () => {
-    const products = [
-        { title: "Dell Latitude G15", image: "https://m.media-amazon.com/images/I/41U-d4vMUqL.__AC_SY300_SX300_QL70_ML2_.jpg", price: 54000, stock: 10},
-    ];
-
-    const existingProducts = await getAllProducts();
-
-    if(existingProducts.length === 0) {
-        await productModel.insertMany(products)
+    try {
+        const products = [
+            { title: "Dell Latitude G15", image: "https://m.media-amazon.com/images/I/41U-d4vMUqL.__AC_SY300_SX300_QL70_ML2_.jpg", price: 54000, stock: 10},
+        ];
+    
+        const existingProducts = await getAllProducts();
+    
+        if(existingProducts.length === 0) {
+            await productModel.insertMany(products)
+        }
+    } catch (err) {
+        console.error("cannot seed database", err)
     }
+    
 };
