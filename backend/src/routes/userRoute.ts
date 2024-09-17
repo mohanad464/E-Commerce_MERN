5be06d442f8342d8508feaf1aa247c215bacf93a
mohanad <mohanad_u@yahoo.com>
2024-09-17 12:20:17 +0300

    Register page

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,26 +1,31 @@
-import express from "express"
+import express from "express";
 import { login, register } from "../services/userServices";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/register', async (request,response) => {
-    try { 
-    const {firstName, lastName, email, password} = request.body;
-    const {statusCode, data} = await register({firstName, lastName, email, password});
-    response.status(statusCode).send(data)
-    } catch (err) {
-        response.status(500).send("Something went wrong!");
-    }
-})
+router.post("/register", async (request, response) => {
+  try {
+    const { firstName, lastName, email, password } = request.body;
+    const { statusCode, data } = await register({
+      firstName,
+      lastName,
+      email,
+      password,
+    });
+    response.status(statusCode).json(data);
+  } catch (err) {
+    response.status(500).send("Something went wrong!");
+  }
+});
 
-router.post('/login', async (request,response) => {
-    try { 
-    const {email, password} = request.body;
-    const {statusCode, data} = await login({email, password});
-    response.status(statusCode).send(data)
-    } catch (err) {
-        response.status(500).send("Something went wrong!");
-    }
-})
+router.post("/login", async (request, response) => {
+  try {
+    const { email, password } = request.body;
+    const { statusCode, data } = await login({ email, password });
+    response.status(statusCode).json(data);
+  } catch (err) {
+    response.status(500).send("Something went wrong!");
+  }
+});
 
 export default router;
---
+++ frontend/src/pages/RegisterPage.tsx
@@ -0,0 +1,92 @@
+import { Box, Container, Typography, TextField, Button } from "@mui/material";
+import { useRef, useState } from "react";
+import { BASE_URL } from "../constants/baseURL";
+
+const RegisterPage = () => {
+  const [error, setError] = useState("");
+  const firstNameRef = useRef<HTMLInputElement>(null);
+  const lastNameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const onSubmit = async () => {
+    const firstName = firstNameRef.current?.value;
+    const lastName = lastNameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    console.log(name, email, password);
+
+    //Make the call to API to create the user
+    const response = await fetch(`${BASE_URL}/user/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+
+    if (!response.ok) {
+      setError("Unable to register user, please try different credientials!");
+      return;
+    }
+
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          mt: 4,
+        }}
+      >
+        <Typography variant="h6">Register New Account</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 2,
+            mt: 2,
+            border: 1,
+            borderColor: "#f5f5f5",
+            padding: 2,
+          }}
+        >
+          <TextField
+            inputRef={firstNameRef}
+            label="First Name"
+            name="firstname"
+          />
+          <TextField inputRef={lastNameRef} label="Last Name" name="lastname" />
+
+          <TextField inputRef={emailRef} label="Email" name="email" />
+
+          <TextField
+            inputRef={passwordRef}
+            type="password"
+            label="Password"
+            name="password"
+          />
+          <Button onClick={onSubmit} variant="contained">
+            Register
+          </Button>
+          {error && <Typography sx={{ color: "red" }}>{error}</Typography>}
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default RegisterPage;
